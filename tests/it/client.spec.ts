c5faa68275833b3e2fd42ebd95c3a1e1e7b061d5
regadas <oss@regadas.email>
2022-07-02 09:14:33 +0100

    Make it tests concurrent

--- tests/it/client.spec.ts
+++ tests/it/client.spec.ts
@@ -1,21 +1,17 @@
 import {BasicAuth, QueryData, Trino} from '../../src';
 
-let trino: Trino;
 const allCustomerQuery = 'select * from customer';
 const limit = 1;
 const singleCustomerQuery = `select * from customer limit ${limit}`;
 const useSchemaQuery = 'use tpcds.sf100000';
 
-beforeEach(() => {
-  trino = new Trino({
-    catalog: 'tpcds',
-    schema: 'sf100000',
-    auth: new BasicAuth('test'),
-  });
-});
-
 describe('trino', () => {
-  test('exhaust query results', async () => {
+  test.concurrent('exhaust query results', async () => {
+    const trino = new Trino({
+      catalog: 'tpcds',
+      schema: 'sf100000',
+      auth: new BasicAuth('test'),
+    });
     const query = await trino.query(singleCustomerQuery);
     const data = await query.fold<QueryData[]>([], (row, acc) => [
       ...acc,
@@ -25,7 +21,12 @@ describe('trino', () => {
     expect(data).toHaveLength(limit);
   });
 
-  test('close running query', async () => {
+  test.concurrent('close running query', async () => {
+    const trino = new Trino({
+      catalog: 'tpcds',
+      schema: 'sf100000',
+      auth: new BasicAuth('test'),
+    });
     const query = await trino.query(allCustomerQuery);
     const qr = await query.next();
     await query.close();
@@ -35,7 +36,12 @@ describe('trino', () => {
     expect(info.state).toBe('FAILED');
   });
 
-  test('cancel running query', async () => {
+  test.concurrent('cancel running query', async () => {
+    const trino = new Trino({
+      catalog: 'tpcds',
+      schema: 'sf100000',
+      auth: new BasicAuth('test'),
+    });
     const query = await trino.query(allCustomerQuery);
     const qr = await query.next();
 
@@ -45,7 +51,12 @@ describe('trino', () => {
     expect(info.state).toBe('FAILED');
   });
 
-  test('get query info', async () => {
+  test.concurrent('get query info', async () => {
+    const trino = new Trino({
+      catalog: 'tpcds',
+      schema: 'sf100000',
+      auth: new BasicAuth('test'),
+    });
     const query = await trino.query(singleCustomerQuery);
     const qr = await query.next();
     await query.close();
@@ -55,8 +66,8 @@ describe('trino', () => {
     expect(info.query).toBe(singleCustomerQuery);
   });
 
-  test('query request header propagation', async () => {
-    trino = new Trino({catalog: 'tpcds', auth: new BasicAuth('test')});
+  test.concurrent('query request header propagation', async () => {
+    const trino = new Trino({catalog: 'tpcds', auth: new BasicAuth('test')});
     const query = await trino.query(useSchemaQuery);
     await query.next();
     await query.close();
