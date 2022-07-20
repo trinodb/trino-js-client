1028ef93d76d30dc90b3438535f3a5597742b935
Filipe Regadas <filiperegadas@gmail.com>
2022-07-20 14:03:35 +0100

    Disable yarn zero-install
    This is mainly due to overall tooling support.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,1 @@
-((typescript-mode
-  . (
-     (eval . (let ((project-directory (car (dir-locals-find-file default-directory))))
-               (setq jest-test-command-string "yarn %s jest %s %s")
-               (setq lsp-eslint-node-path (concat project-directory ".yarn/sdks"))
-               (setq lsp-clients-typescript-server-args `("--tsserver-path" ,(concat project-directory ".yarn/sdks/typescript/bin/tsserver") "--stdio")))))))
+((typescript-mode . ((jest-test-command-string "yarn %s jest %s %s"))))
